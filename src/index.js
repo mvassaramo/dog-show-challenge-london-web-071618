0616c84a59eba53e3a0c99b5b8cff58044286b38
github username <github email address>
2018-09-12 10:36:18 +0100

    Added save form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
+  function getData() {
+    return fetch('http://localhost:3000/dogs')
+    .then(resp => resp.json())
+    .then(dogs => dogs.forEach(dog => appendDog(dog)))
+  }
+
+  getData()
+
+  function appendDog(dog){
+
+    const tableBodyEl = document.querySelector('#table-body')
+    const tableRowEl = tableBodyEl.insertRow(0)
+      tableRowEl.innerHTML = `
+      <tr><td>${dog.name}</td>
+      <td>${dog.breed}</td>
+      <td>${dog.sex}</td>
+      <td><button>Edit</button></td></tr>
+      `
+    tableBodyEl.append(tableRowEl)
+
+    const editButton = document.querySelector('button')
+
+
+    editButton.addEventListener('click', (event) => {
+      console.log('works!')
+    })
+
+
+  }
+
+
+
+
+
+
 })
